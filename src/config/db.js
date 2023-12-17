652480f79f9b7c6b9d5249b7bf8bb6176afd782f
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2023-12-17 16:25:41 +0530

    updated mongodb link

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const baseUrl = process.env.MONGODB || "0.0.0.0:27017";
+const url = "mongodb+srv://amal:suvarnam123@cluster0.0ywkcio.mongodb.net/?retryWrites=true&w=majority";
 export const connectToDb=async()=>{
     try {
-            await mongoose.connect(`mongodb://${baseUrl}/pollingSystem`, {
+            await mongoose.connect(url, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             });
